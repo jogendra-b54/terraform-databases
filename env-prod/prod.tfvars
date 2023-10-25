f704ee43a4a85b965b818d77bff892b140a728ef
jogendra-b54 <jpatro.kumar@gmail.com>
2023-10-25 18:33:33 +0530

    Auto push

--- env-prod/prod.tfvars
+++ env-prod/prod.tfvars
@@ -1,1 +1,9 @@
 ENV               = "prod"
+
+
+# RDS variables
+
+MYSQL_PORT_NUMBER     = 3306
+MYSQL_STORAGE         = 10
+MYSQL_ENGINE_VERSION  ="5.7"
+MYSQL_INSTANCE_TYPE   = "db.t3.medium" 
--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 module "docdb" {
-  source              = "./vendor/modules/docdb"
-  ENV                 = var.ENV
+  source                = "./vendor/modules/docdb"
+  ENV                   = var.ENV
+  MYSQL_PORT_NUMBER     = var.MYSQL_PORT_NUMBER
+  MYSQL_STORAGE         = var.MYSQL_STORAGE
+  MYSQL_ENGINE_VERSION  = var.MYSQL_ENGINE_VERSION
+  MYSQL_INSTANCE_TYPE   =  var.MYSQL_INSTANCE_TYPE
 }
 
 module "redis" {
