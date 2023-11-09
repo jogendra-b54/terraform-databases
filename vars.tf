427260b62d1f3a19ce6edc8b3b6f95de90d49d0d
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-09 18:47:40 +0530

    Auto push

--- vars.tf
+++ vars.tf
@@ -1,10 +1,10 @@
 variable "ENV" {}
 
-#MYSQL Variables
-variable "MYSQL_PORT_NUMBER" {}
-variable "MYSQL_STORAGE" {}
-variable "MYSQL_ENGINE_VERSION" {}
-variable "MYSQL_INSTANCE_TYPE" {}
+# #MYSQL Variables
+# variable "MYSQL_PORT_NUMBER" {}
+# variable "MYSQL_STORAGE" {}
+# variable "MYSQL_ENGINE_VERSION" {}
+# variable "MYSQL_INSTANCE_TYPE" {}
 
 #DocDB Variables
 variable "DOCDB_PORT_NUMBER" {}
@@ -12,13 +12,13 @@ variable "DOCDB_INSTANCE_TYPE" {}
 variable "DOCDB_INSTANCE_COUNT" {}
 
 
-#Redis variables
-variable "REDIS_PORT_NUMBER" {}
-variable "REDIS_INSTANCE_TYPE" {}
-variable "REDIS_INSTANCE_COUNT" {}
-variable "REDIS_ENGINE_VERSION" {}
+# #Redis variables
+# variable "REDIS_PORT_NUMBER" {}
+# variable "REDIS_INSTANCE_TYPE" {}
+# variable "REDIS_INSTANCE_COUNT" {}
+# variable "REDIS_ENGINE_VERSION" {}
 
-#RabbitMQ variables
-variable "RABBITMQ_PORT_NUMBER" {}
-variable "RABBITMQ_INSTANCE_TYPE" {}
+# #RabbitMQ variables
+# variable "RABBITMQ_PORT_NUMBER" {}
+# variable "RABBITMQ_INSTANCE_TYPE" {}
 
