6707fe3cc8e9d5cbefcde337ee4c994a27a6a64a
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-09 19:01:44 +0530

    Auto push

--- vars.tf
+++ vars.tf
@@ -7,9 +7,9 @@ variable "ENV" {}
 # variable "MYSQL_INSTANCE_TYPE" {}
 
 #DocDB Variables
-variable "DOCDB_PORT_NUMBER" {}
-variable "DOCDB_INSTANCE_TYPE" {}
-variable "DOCDB_INSTANCE_COUNT" {}
+#variable "DOCDB_PORT_NUMBER" {}
+#variable "DOCDB_INSTANCE_TYPE" {}
+#variable "DOCDB_INSTANCE_COUNT" {}
 
 
 # #Redis variables
