2bda94e76173d7fa36875e9a6c394bf3a3f6ec30
jogendra-b54 <jpatro.kumar@gmail.com>
2023-11-10 12:42:11 +0530

    Auto push

--- main.tf
+++ main.tf
@@ -19,10 +19,10 @@ module "docdb" {
  module "mysql" {
    source              = "./vendor/modules/mysql"
    ENV                 = var.ENV
-#     MYSQL_PORT_NUMBER     = var.MYSQL_PORT_NUMBER
-#   MYSQL_STORAGE         = var.MYSQL_STORAGE
-#   MYSQL_ENGINE_VERSION  = var.MYSQL_ENGINE_VERSION
-#   MYSQL_INSTANCE_TYPE   =  var.MYSQL_INSTANCE_TYPE
+    MYSQL_PORT_NUMBER     = var.MYSQL_PORT_NUMBER
+   MYSQL_STORAGE         = var.MYSQL_STORAGE
+   MYSQL_ENGINE_VERSION  = var.MYSQL_ENGINE_VERSION
+   MYSQL_INSTANCE_TYPE   =  var.MYSQL_INSTANCE_TYPE
  }
 
 # module "rabbitmq" {
